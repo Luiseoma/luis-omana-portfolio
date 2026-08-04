bd2994a2414bd3b3a29aaa9f1d8cb3c15533c0bb
Luiseoma <luisenriqueoma@gmail.com>
2026-08-04 16:56:42 -0400

    muchos avances

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,60 @@ if (menuToggle && navLinks) {
     });
 }
 
+const scrollTop = document.getElementById('scrollTop');
+
+if (scrollTop) {
+    window.addEventListener('scroll', () => {
+
+        if (window.scrollY > 300) {
+            scrollTop.classList.add('show');
+        } else {
+            scrollTop.classList.remove('show');
+        }
+
+    });
+}
+
+const sections = document.querySelectorAll('section');
+const navLinksActive = document.querySelectorAll('.nav-links a');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    if(window.scrollY < 200){
+        current = "hero";
+    }
+    
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        
+        if (window.scrollY >= sectionTop - 200 &&
+            window.scrollY < sectionTop + sectionHeight - 200) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLinksActive.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href') === "#" + current) {
+            link.classList.add('active');
+        }
+    });
+});
+
+const animatedElements = document.querySelectorAll(".fade-in, .slide-left, .slide-right, .slide-up");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show-animation");
+        }
+    });
+
+}, {
+    threshold: 0.2
+});
+
+animatedElements.forEach(element => {
+    observer.observe(element);
+});
